a77708b6fc540556a5829d102b267c3673de5f5f
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2025-07-13 10:46:07 +0000

    restrict required oauth permissions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'openid email profile https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/calendar.readonly'
+          scope: 'openid email profile'
         }
       }
     }),
